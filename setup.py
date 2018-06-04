3fa7f3fa4ad35520616ba16fa0a10326e30f9730
Dan MacKinlay <myopenid@email.possumpalace.org>
2018-06-04 10:08:41 +1000

    prepare for PyPI upload

--- setup.py
+++ setup.py
@@ -10,9 +10,20 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 setup(
       name='latex_fragment',
       version='0.0.1',
-      description=u"Skeleton of a Python package",
+      description=u"Display and output fragments of LaTeX markup as vector graphics",
       long_description=long_description,
-      classifiers=[],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: IPython",
+        "Framework :: Jupyter",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+        "Topic :: Scientific/Engineering :: Mathematics",
+"Topic :: Text Processing :: Markup :: LaTeX"
+      ],
       keywords='',
       author=u"Dan MacKinlay",
       author_email='dan@livingthing.org',
